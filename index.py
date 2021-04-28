8127ab7ee19d8f19cd36ca46ae29748d1710aa0f
[nick varner] <nick.varner@gmail.com>
2021-04-28 14:23:00 -0700

    initialized

---
+++ index.py
@@ -0,0 +1,13 @@
+# This program says hello and asks for my name
+
+print('hello world')
+
+print('what is your name?')
+myName = input()
+print('it is good to met you ' + myName)
+print('the length of your name is: ')
+
+print(len(myName))
+print('what is your age?')
+myAge = input()
+print('You will be '+ str(int(myAge) + 1) + ' in a year.')
